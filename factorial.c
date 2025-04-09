615bb0626dcdb3e0e03a7c2bcc3d8d460c7491bc
swathi9030 <skola2@student.gitam.edu>
2025-04-09 14:41:09 +0530

    factorial.c

---
+++ factorial.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    unsigned long long fact = 1;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; ++i) {
+        fact *= i;
+    }
+
+    printf("Factorial of %d = %llu\n", n, fact);
+    return 0;
+}
+
+
+
+OUTPUT :
+Enter a positive integer: 5
+Factorial of 5 = 120
